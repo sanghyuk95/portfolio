1bec8cd4a8459395a871451a1e04c405bed32ba1
Burny95 <shskse5@gmail.com>
2023-03-02 15:03:14 +0900

    03-02 포폴까지만

--- js/index.js
+++ js/index.js
@@ -24,6 +24,11 @@ new Swiper(".mySwiper2", {
   freeMode: true,
 });
 new Swiper(".mySwiper3", {
+  slidesPerView: 2.2,
+  spaceBetween: 10,
+  freeMode: true,
+});
+new Swiper(".mySwiper4", {
   slidesPerView: 1.5,
   spaceBetween: 10,
   freeMode: true,
