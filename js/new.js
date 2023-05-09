01ca7ec6758a447559a9d3102c8f2f7a5d65d1cd
Burny95 <shskse5@gmail.com>
2023-05-10 00:31:30 +0900

    반응형,솔로부분 수정

--- js/new.js
+++ js/new.js
@@ -29,18 +29,12 @@ $choiceSolo.forEach((item,idx) => {
 const $mast = document.querySelector('.mast')
 const $mastDetail = document.querySelector('.mast-detail')
 const $modalX = document.querySelectorAll('.modal-x-btn')
-const $weather = document.querySelector('.weather')
-const $weatherDetail = document.querySelector('.weather-detail')
 
 $mastDetail.addEventListener('click', () => {
   $mast.classList.remove('active')
 })
-$weatherDetail.addEventListener('click', () => {
-  $weather.classList.remove('active')
-})
 $modalX.forEach((item) => {  
   item.addEventListener('click', () => {
     $mast.classList.add('active')
-    $weather.classList.add('active')
   })
 })
